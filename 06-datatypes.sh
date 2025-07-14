73dbc239b63c7b92688e66420b7004c682da39d3
sivakumar <gsivadevops@gmail.com>
2025-07-14 19:17:46 +0530

    datatypes created

---
+++ 06-datatypes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NUMBER1=100
+NUMBER2=200
+
+echo "datatyeps introduction"
+
+TIMESTAMP=$(date) #date is a command, storing the output in TIMESTAMP
+
+echo "Script executed at : $TIMESTAMP"
+SUM = $(($NUMBER1+$NUMBER2))
+
+echo "Sum of $NUMBER1 and $NUMBER2 is: $SUM
+
