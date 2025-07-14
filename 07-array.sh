1c0c89891c764f084dd9c6cc4a1ffb8289b56483
sivakumar <gsivadevops@gmail.com>
2025-07-14 19:45:44 +0530

    array chnages added

--- 07-array.sh
+++ 07-array.sh
@@ -3,7 +3,11 @@
 MOVIES=("court" "hit3" "pushpa2" "thandel")
 
 echo "first movie : ${MOVIES[0]}"
+
 echo "All movies : ${MOVIES[@]}"
+
 echo "first movie : ${MOVIES[1]}"
+
 echo "third movie : ${MOVIES[3]}"
+
 echo "fourth movie : ${MOVIES[4]}"
