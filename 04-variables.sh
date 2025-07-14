304aa50d7e17b0d51f23c718870e2c695e18f81e
sivakumar <gsivadevops@gmail.com>
2025-07-14 18:57:10 +0530

    variables with arguments

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hey $PERSON2, how are you?"
+echo "$PERSON2: Hey $PERSON1, I am fine, How are you doing"
+echo "$PERSON1: I am doing good, Whats up?"
+echo "$PERSON2: Nothing"
