d917b438897d69bf9e266e1f15d3c3a66060a010
sivakumar <gsivadevops@gmail.com>
2025-07-14 19:34:45 +0530

    array changes

--- 07-array.sh
+++ 07-array.sh
@@ -3,9 +3,6 @@
 MOVIES=("court" "hit3" "pushpa2" "thandel")
 
 echo "first movie : ${MOVIES[0]}"
-
 echo "All movies : ${MOVIES[@]}"
-
 ech0 "fourth movie : ${MOVIES[4]}"
-
 ech0 "third movie : ${MOVIES[3]}"
