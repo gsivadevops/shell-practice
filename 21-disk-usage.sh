2135a149c4e9745072fbb6499882901842feed4a
sivakumar <gsivadevops@gmail.com>
2025-08-04 16:40:43 +0530

    disk usage changes

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_USAGE=$(df -hT | grep -v Filesystem) # grep -v is for opposit search to get all other than Filesystem
 DISK_THRESHOLD=1 # in project it will be 75
 MSG=""
 IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
